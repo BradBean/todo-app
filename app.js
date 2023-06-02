599d740319c1ea38510b70a9cec8e89f0a3bcfee
Brad Bean <bradbean@brads-mbp.lan>
2023-06-02 11:00:55 -0600

    Added the ability to make a new list of todo's and alternate between them.

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
+document.getElementById('addListButton').addEventListener('click', function() {
+    const listInput = document.getElementById('newListInput');
+    const newList = listInput.value;
+    listInput.value = '';
+
+    const listSelection = document.getElementById('listDropdown');
+
+    const listOption = document.createElement('option');
+    listOption.textContent = newList;
+    listOption.value = newList;
+
+    listSelection.appendChild(listOption);
+
+    document.getElementById('taskInputSection').style.display = 'block';
+    document.getElementById('createListMessage').style.display = 'none';
+
+    const taskList = document.createElement('div');
+    taskList.id = newList;
+    taskList.style.display = 'none';
+
+    document.getElementById('taskLists').appendChild(taskList);
+});
+
 document.getElementById('addTaskButton').addEventListener('click', function() {
     const taskInput = document.getElementById('newTaskInput');
     const newTask = taskInput.value;
     taskInput.value = '';
 
-    const taskList = document.getElementById('taskLists');
+    const selectedList = document.getElementById('listDropdown').value;
+    const taskList = document.getElementById(selectedList);
 
     const taskItem = document.createElement('div');
-    taskItem.className = 'task-item';
+    taskItem.textContent = newTask;
+    taskItem.className = 'task-item'; 
 
-    const taskName = document.createElement('span');
-    taskName.textContent = newTask;
-
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Delete';
-    deleteButton.className = 'btn btn-danger';
-    deleteButton.addEventListener('click', function() {
-        taskList.removeChild(taskItem);
-    });
-
-    taskItem.appendChild(taskName);
-    taskItem.appendChild(deleteButton);
     taskList.appendChild(taskItem);
 });
