f4dc347c11d735dd034078877ce547ba5b344a65
Brad Bean <bradbean@brads-mbp.lan>
2023-06-02 11:28:22 -0600

    Refactored list display, added task completion and removal features, and fixed task rendering bug.

--- app.js
+++ app.js
@@ -3,19 +3,24 @@ document.getElementById('addListButton').addEventListener('click', function() {
     const newList = listInput.value;
     listInput.value = '';
 
-    const listSelection = document.getElementById('listDropdown');
+    const listSelection = document.getElementById('listSelection');
 
-    const listOption = document.createElement('option');
-    listOption.textContent = newList;
-    listOption.value = newList;
+    const listDiv = document.createElement('div');
+    listDiv.textContent = newList;
+    listDiv.id = newList;
+    listDiv.className = 'list-item';
+    listDiv.onclick = function() {
+        switchList(newList);
+    };
 
-    listSelection.appendChild(listOption);
+    listSelection.appendChild(listDiv);
 
-    document.getElementById('taskInputSection').style.display = 'block';
+    document.getElementById('taskInputSection').style.display = 'none';
+    document.getElementById('selectListMessage').style.display = 'block';
     document.getElementById('createListMessage').style.display = 'none';
 
     const taskList = document.createElement('div');
-    taskList.id = newList;
+    taskList.id = newList + '-tasks';
     taskList.style.display = 'none';
 
     document.getElementById('taskLists').appendChild(taskList);
@@ -26,12 +31,53 @@ document.getElementById('addTaskButton').addEventListener('click', function() {
     const newTask = taskInput.value;
     taskInput.value = '';
 
-    const selectedList = document.getElementById('listDropdown').value;
-    const taskList = document.getElementById(selectedList);
+    const selectedList = document.querySelector('.list-item.selected').id;
+    const taskList = document.getElementById(selectedList + '-tasks');
 
     const taskItem = document.createElement('div');
-    taskItem.textContent = newTask;
-    taskItem.className = 'task-item'; 
+    taskItem.className = 'task-item';
+
+    const taskText = document.createElement('span');
+    taskText.textContent = newTask;
+
+    const completeButton = document.createElement('button');
+    completeButton.textContent = 'Complete';
+    completeButton.className = 'btn btn-success complete-btn';
+    completeButton.onclick = function() {
+        taskText.style.textDecoration = 'line-through';
+    };
+
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'Remove';
+    removeButton.className = 'btn btn-danger remove-btn';
+    removeButton.onclick = function() {
+        taskItem.remove();
+    };
+
+    taskItem.appendChild(taskText);
+    taskItem.appendChild(completeButton);
+    taskItem.appendChild(removeButton);
 
     taskList.appendChild(taskItem);
 });
+
+function switchList(listId) {
+    const taskLists = document.getElementById('taskLists').children;
+    for(let i=0; i<taskLists.length; i++) {
+        taskLists[i].style.display = 'none';
+    }
+
+    document.getElementById(listId + '-tasks').style.display = 'block';
+
+    const listItems = document.getElementById('listSelection').children;
+    for(let i=0; i<listItems.length; i++) {
+        if(listItems[i].id === listId) {
+            listItems[i].classList.add('selected');
+        } else {
+            listItems[i].classList.remove('selected');
+        }
+    }
+
+    document.getElementById('taskInputSection').style.display = 'block';
+    document.getElementById('selectListMessage').style.display = 'none';
+}
